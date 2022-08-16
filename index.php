45aa9967783d6744609dd9b2cbeffd064ce738f0
Yaasir Cheekoory <yaasir1997@gmail.com>
2022-08-16 21:10:18 +0400

    added wording

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="style.css">
-<title>Ufesa Drawing Contest</title>
+<title>Drawing Contest</title>
 </head>
 
 <body>
